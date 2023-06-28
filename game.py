710a2a5e7b3a7892d97a8349f18c73241abf3e44
waweroh <scadmsez@gmail.com>
2023-06-28 10:10:23 +0300

    initializing the game window and the speed level

--- game.py
+++ game.py
@@ -15,3 +15,13 @@ white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
+
+# Initialising pygame
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('GeeksforGeeks Snakes')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
