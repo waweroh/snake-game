11d6e10db36d6179fc6345a6889d6e173f4296a9
waweroh <scadmsez@gmail.com>
2023-06-29 10:32:36 +0300

    define snake and fruit position

--- game.py
+++ game.py
@@ -25,3 +25,21 @@ game_window = pygame.display.set_mode((window_x, window_y))
 
 # FPS (frames per second) controller
 fps = pygame.time.Clock()
+
+# Snake position
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake body
+snake_body = [[100, 50],
+			[90, 50],
+			[80, 50],
+			[70, 50]
+			]
+# fruit position
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+				random.randrange(1, (window_y//10)) * 10]
+
+fruit_spawn = True
+
+# setting default snake direction towards
+# right
