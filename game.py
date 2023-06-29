9a69972a3651f711392aaabd22f08be3cbfc1d49
waweroh <scadmsez@gmail.com>
2023-06-29 13:57:58 +0300

    show score function

--- game.py
+++ game.py
@@ -48,3 +48,20 @@ change_to = direction
 
 # initial score
 score = 0
+
+# displaying Score function
+def show_score(choice, color, font, size):
+
+	# creating font object score_font
+	score_font = pygame.font.SysFont(font, size)
+	
+	# create the display surface object
+	# score_surface
+	score_surface = score_font.render('Score : ' + str(score), True, color)
+	
+	# create a rectangular object for the text
+	# surface object
+	score_rect = score_surface.get_rect()
+	
+	# displaying text
+	game_window.blit(score_surface, score_rect)
